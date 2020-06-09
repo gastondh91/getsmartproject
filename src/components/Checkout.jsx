8eda53157e99c9b3814cdf4677751c67561db255
gaston <gastondh91@gmail.com>
2020-06-09 18:42:10 -0300

    cambio de practicas

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,181 +1,221 @@
-import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
-import { getOrdenes } from '../redux/action-creators/action-creator';
-import { fetchCarrito } from '../redux/action-creators/carrito-actions';
-import axios from 'axios';
-import ModalInfo from './ModalInfo';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+import { connect } from 'react-redux'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+import { getOrdenes } from '../redux/action-creators/action-creator'
+import { fetchCarrito } from '../redux/action-creators/carrito-actions'
+import ModalInfo from './ModalInfo'
 
 const Checkout = (props) => {
 
-  var [inputs, setInputs] = useState({});
-  var [cantidades, setCantidades] = useState({});
-  var [modal, setModal] = useState(false);
+  const [inputs, setInputs] = useState({})
+  let [cantidades, setCantidades] = useState({})
+  let [modal, setModal] = useState(false)
 
   const numberToString = (numero) => {
-    let string = numero.toString();
-    if (string.length == 4) return string[0] + '.' + string.slice(1);
-    if (string.length == 5) return string.slice(0, 2) + '.' + string.slice(2);
-    if (string.length == 6) return string.slice(0, 3) + '.' + string.slice(3);
-    if (string.length == 7) return string.slice(0, 4) + '.' + string.slice(4);
-  };
+    const string = numero.toString()
+    if (string.length === 4) return `${string[0]}.${string.slice(1)}`
+    if (string.length === 5) return `${string.slice(0, 2)}.${string.slice(2)}`
+    if (string.length === 6) return `${string.slice(0, 3)}.${string.slice(3)}`
+    if (string.length === 7) return `${string.slice(0, 4)}.${string.slice(4)}`
+  }
 
   const priceToNumber = (numero) => {
-    var splited = numero.split('.');
-    var number = Number(splited.join(''));
-    return number;
-  };
+    const splited = numero.split('.')
+    const number = Number(splited.join(''))
+    return number
+  }
 
-  var total = 0;
-  var cantidadTotal = 0;
-  var SumaTotal = 0;
+  let total = 0
+  let cantidadTotal = 0
+  let SumaTotal = 0
 
-  var funcCantidades = (cantidad) => {
-    cantidadTotal += cantidad;
-    return cantidad;
-  };
+  const funcCantidades = (cantidad) => {
+    cantidadTotal += cantidad
+    return cantidad
+  }
 
   const subtotal = (precio, cantidad) => {
-    var sumatoria = priceToNumber(precio) * cantidad;
-    total += sumatoria;
-    return sumatoria;
-  };
+    const sumatoria = priceToNumber(precio) * cantidad
+    total += sumatoria
+    return sumatoria
+  }
 
   const Totales = (precio, cantidad) => {
-    let subTotal = priceToNumber(precio) * cantidad;
-    SumaTotal += subtotal(precio, cantidad);
-    return numberToString(subTotal);
-  };
+    const subTotal = priceToNumber(precio) * cantidad
+    SumaTotal += subtotal(precio, cantidad)
+    return numberToString(subTotal)
+  }
 
   const separarCantidades = (productos) => {
-    let cantidad = {};
+    const cantidad = {}
     for (let i = 0; i < productos.length; i += 1) {
-      cantidad[productos[i].id] = productos[i].carrito.cantidad;
+      cantidad[productos[i].id] = productos[i].carrito.cantidad
     }
-    setCantidades(cantidades = cantidad);
-  };
+    setCantidades(cantidades = cantidad)
+  }
 
 
   useEffect(() => {
 
-    props.getOrdenes(props.usuario.id);
+    props.getOrdenes(props.usuario.id)
 
-    props.fetchCarrito(props.usuario.id);
+    props.fetchCarrito(props.usuario.id)
 
-    setInputs({ nombreyapellido: props.usuario.nombre + ' ' + props.usuario.apellido, email: props.usuario.email, direccion: props.usuario.domicilio });
-  }, []);
+    setInputs({ nombreyapellido: `${props.usuario.nombre} ${props.usuario.apellido}`, email: props.usuario.email, direccion: props.usuario.domicilio })
+  }, [])
 
   const handleChange = () => {
-    setInputs(inputs => ({ ...inputs, [event.target.name]: event.target.value }));
-  };
+    setInputs(inputs => ({ ...inputs, [event.target.name]: event.target.value }))
+  }
 
   const handleSubmit = () => {
 
     if (!inputs.nombreyapellido || !inputs.email || !inputs.direccion || !inputs.cp || !inputs.localidad) {
-      setModal(modal = ['Error', 'Debes completar todos los campos']);
-      return;
-    }
-    else {
-      let ordenes = props.ordComp;
-      let repetido;
+      setModal(modal = ['Error', 'Debes completar todos los campos'])
+
+    } else {
+      const ordenes = props.ordComp
+      let repetido
       for (let i = 0; i < ordenes.length; i += 1) {
-        if ((ordenes[i].usuarioId == props.usuario.id) && ordenes[i].status == 'CREADO') {
-          repetido = true;
+        if ((ordenes[i].usuarioId === props.usuario.id) && ordenes[i].status === 'CREADO') {
+          repetido = true
         }
       }
       if (repetido) {
-        setModal(modal = ['Datos cargados', 'Se creó la orden de compra']);
-      }
-      else {
-        separarCantidades(props.carrito);
+        setModal(modal = ['Datos cargados', 'Se creó la orden de compra'])
+      } else {
+        separarCantidades(props.carrito)
 
-        axios.post(`/api/ordencompra/crear`, {
+        axios.post('/api/ordencompra/crear', {
           datos: inputs,
           usuario: props.usuario.id,
           productos: props.carrito.map(producto => producto.id),
           total: numberToString(SumaTotal),
-          cantidades
+          cantidades,
         })
-          .then(() => setModal(modal = ['Datos cargados', 'Se creó la orden de compra']));
+          .then(() => setModal(modal = ['Datos cargados', 'Se creó la orden de compra']))
       }
     }
-  };
+  }
 
   return (
     <div>
-      <div className='checkoutview datosgrid'>
+      <div className="checkoutview datosgrid">
         <div>
           <div style={{ textAlign: 'center' }} className="cardcheck cardCheckout">
             <div style={{ fontWeight: '600', fontSize: '1.2rem' }} className="card-header">
               Datos del Cliente
-        </div>
+            </div>
             <ul style={{ maxWidth: '100%' }} className="list-group list-group-flush">
-              <li className="list-group-item "><p className='pCheckout' >Nombre y apellido:</p> <input style={{ textAlign: 'center' }} onChange={handleChange} defaultValue={props.usuario.nombre + ' ' + props.usuario.apellido} type="text" name="nombreyapellido" className='inputDetail' /></li>
-              <li className="list-group-item"><p className='pCheckout' >Email:</p> <input style={{ textAlign: 'center' }} onChange={handleChange} defaultValue={props.usuario.email} type="email" name="email" className='inputDetail' /></li>
-              <li className="list-group-item"><p className='pCheckout' >Dirección de envío:</p> <input style={{ textAlign: 'center' }} onChange={handleChange} defaultValue={props.usuario.domicilio ? props.usuario.domicilio : ''} type="text" name="direccion" className='inputDetail' /></li>
-              <li className="list-group-item"><p className='pCheckout' >Código Postal:</p> <input style={{ textAlign: 'center' }} onChange={handleChange} type="text" name="cp" className='inputDetail' /></li>
-              <li className="list-group-item"><p className='pCheckout' >Localidad:</p> <input style={{ textAlign: 'center' }} onChange={handleChange} type="text" name="localidad" className='inputDetail' /></li>
+              <li className="list-group-item ">
+                <p className="pCheckout">Nombre y apellido:</p>
+                {' '}
+                <input style={{ textAlign: 'center' }} onChange={handleChange} defaultValue={`${props.usuario.nombre} ${props.usuario.apellido}`} type="text" name="nombreyapellido" className="inputDetail" />
+              </li>
+              <li className="list-group-item">
+                <p className="pCheckout">Email:</p>
+                {' '}
+                <input style={{ textAlign: 'center' }} onChange={handleChange} defaultValue={props.usuario.email} type="email" name="email" className="inputDetail" />
+              </li>
+              <li className="list-group-item">
+                <p className="pCheckout">Dirección de envío:</p>
+                {' '}
+                <input style={{ textAlign: 'center' }} onChange={handleChange} defaultValue={props.usuario.domicilio ? props.usuario.domicilio : ''} type="text" name="direccion" className="inputDetail" />
+              </li>
+              <li className="list-group-item">
+                <p className="pCheckout">Código Postal:</p>
+                {' '}
+                <input style={{ textAlign: 'center' }} onChange={handleChange} type="text" name="cp" className="inputDetail" />
+              </li>
+              <li className="list-group-item">
+                <p className="pCheckout">Localidad:</p>
+                {' '}
+                <input style={{ textAlign: 'center' }} onChange={handleChange} type="text" name="localidad" className="inputDetail" />
+              </li>
             </ul>
           </div>
         </div>
-        {<ModalInfo
+        <ModalInfo
           encabezado={modal ? modal[0] : ''}
           accion={modal ? modal[1] : ''}
           history={props.history}
-          historypush={modal[0] == 'Error' ? '/pagos' : '/tarjeta'}
-        />}
+          historypush={modal[0] === 'Error' ? '/pagos' : '/tarjeta'}
+        />
         <div>
 
           <div>
             <h2 style={{ textAlign: 'center', marginTop: '1.30rem' }}> Detalles de la compra:</h2>
-            <table className='tabla'>
+            <table className="tabla">
               <tbody>
-                <tr className='tr'>
+                <tr className="tr">
                   <th>Imagenes</th>
                   <th>Productos</th>
                   <th>Cantidad</th>
                   <th>Precio unitario</th>
                   <th>Subtotal</th>
                 </tr>
-                {props.carrito.map(producto => {
-                  return (
-                    <tr key={producto.id}>
-                      <td><img style={{ width: '5rem', height: '5rem', objectFit: 'contain' }} src={producto.imagenes[0]} /></td>
-                      <td><Link className='LinkCheckout' to={`/productos/${producto.id}`}>{producto.marca + ' ' + producto.modelo}</Link></td>
-                      <td>{funcCantidades(producto.carrito.cantidad)}</td>
-                      <td>${producto.precio}</td>
-                      <td>${Totales(producto.precio, producto.carrito.cantidad)}</td>
-                    </tr>);
-                })}
-                <tr style={{ borderTop: 'solid thin darkslategrey' }}><td /><td /><td style={{ fontWeight: '600' }}>{cantidadTotal}</td><td /><td style={{ fontWeight: '600' }}>${numberToString(total)}</td></tr>
+                {props.carrito.map(producto => (
+                  <tr key={producto.id}>
+                    <td><img style={{ width: '5rem', height: '5rem', objectFit: 'contain' }} src={producto.imagenes[0]} /></td>
+                    <td><Link className="LinkCheckout" to={`/productos/${producto.id}`}>{`${producto.marca} ${producto.modelo}`}</Link></td>
+                    <td>{funcCantidades(producto.carrito.cantidad)}</td>
+                    <td>
+                      $
+                      {producto.precio}
+                    </td>
+                    <td>
+                      $
+                      {Totales(producto.precio, producto.carrito.cantidad)}
+                    </td>
+                  </tr>
+                ))}
+                <tr style={{ borderTop: 'solid thin darkslategrey' }}>
+                  <td />
+                  <td />
+                  <td style={{ fontWeight: '600' }}>{cantidadTotal}</td>
+                  <td />
+                  <td style={{ fontWeight: '600' }}>
+                    $
+                    {numberToString(total)}
+                  </td>
+                </tr>
               </tbody>
             </table>
           </div>
           <button
             onClick={handleSubmit}
-            data-toggle='modal'
-            data-target='#infoModal'
-            style={{ marginBottom: '2rem' }} className='example_b general' id='checkoutButton' >Proceder al pago</button>
+            data-toggle="modal"
+            data-target="#infoModal"
+            style={{ marginBottom: '2rem' }}
+            className="example_b general"
+            id="checkoutButton"
+          >
+            Proceder al pago
+          </button>
           <button
             onClick={() => props.history.push('/cart')}
-            className='example_c general'
-            id='goBack'
-          >Volver al carrito</button>
+            className="example_c general"
+            id="goBack"
+          >
+            Volver al carrito
+          </button>
         </div>
       </div>
-    </div>);
-};
+    </div>
+  )
+}
 
 
 const mapStateToProps = (state) => ({
   usuario: state.usuario,
   carrito: state.carrito,
-  ordComp: state.ordComp
-});
+  ordComp: state.ordComp,
+})
 
 const mapDispatchToProps = (dispatch) => ({
   fetchCarrito: (id) => dispatch(fetchCarrito(id)),
-  getOrdenes: (id) => dispatch(getOrdenes(id))
-});
+  getOrdenes: (id) => dispatch(getOrdenes(id)),
+})
 
-export default connect(mapStateToProps, mapDispatchToProps)(Checkout);
+export default connect(mapStateToProps, mapDispatchToProps)(Checkout)
--- src/containers/CarritoContainer.jsx
+++ src/containers/CarritoContainer.jsx
@@ -1,41 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import { fetchCarrito, updateCarrito } from '../redux/action-creators/carrito-actions.js';
-import { connect } from 'react-redux';
+import React, { useEffect, useState } from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
+import { fetchCarrito, updateCarrito } from '../redux/action-creators/carrito-actions.js'
 
 const CarritoContainer = (props) => {
 
-  const [inputs, setInputs] = useState({});
-  var [Total, setTotal] = useState(0)
+  const [inputs, setInputs] = useState({})
+  let [Total, setTotal] = useState(0)
 
 
   useEffect(() => {
-    if (props.usuario) props.getCarrito(props.usuario.id);
+    if (props.usuario) props.getCarrito(props.usuario.id)
 
   }, [])
 
 
-
-
   useEffect(() => {
     if (props.carrito) {
-      var totalMap = props.carrito.map(producto => stringToNumber(producto.precio) * (inputs[producto.id] ? inputs[producto.id] : producto.carrito.cantidad));
-      setTotal(Total = totalMap.reduce((a, b) => {return a + b},0))
+      const totalMap = props.carrito.map(producto => stringToNumber(producto.precio) * (inputs[producto.id] ? inputs[producto.id] : producto.carrito.cantidad))
+      setTotal(Total = totalMap.reduce((a, b) => a + b, 0))
     }
   }, [inputs])
 
 
-const elTotal = (carrito)=>{
-  var total =carrito.map(producto => stringToNumber(producto.precio) * producto.carrito.cantidad);
-  
-  total = total.reduce( (a,b)=> {
-    return (a+b)
+  const elTotal = (carrito) => {
+    let total = carrito.map(producto => stringToNumber(producto.precio) * producto.carrito.cantidad)
 
-  },0)
-  return total
-}
-
+    total = total.reduce((a, b) => (a + b), 0)
+    return total
+  }
 
 
   const stringToNumber = (string) => {
@@ -44,15 +38,15 @@ const elTotal = (carrito)=>{
     return numero
   }
 
-  const numberToString = (numero)=>{
-    let string = numero.toString()
-    if(string.length == 4) return string[0] + '.' + string.slice(1)
-    if(string.length == 5) return string.slice(0,2) + '.' + string.slice(2)
-    if(string.length == 6) return string.slice(0,3) + '.' + string.slice(3)
+  const numberToString = (numero) => {
+    const string = numero.toString()
+    if (string.length == 4) return `${string[0]}.${string.slice(1)}`
+    if (string.length == 5) return `${string.slice(0, 2)}.${string.slice(2)}`
+    if (string.length == 6) return `${string.slice(0, 3)}.${string.slice(3)}`
   }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault()
     if (Object.keys(inputs).length) CartPromise(props.usuario.id, inputs, props.carrito)
     else props.history.push('/pagos')
   }
@@ -62,11 +56,11 @@ const elTotal = (carrito)=>{
       props.updateCarrito(usuario, input, producto)
       resolve('ok')
     })
-    .then(() => props.history.push('/pagos'))
+      .then(() => props.history.push('/pagos'))
   }
 
   const handleChange = (e) => {
-    setInputs(inputs => ({ ...inputs, [event.target.name]: Number(event.target.value) }));
+    setInputs(inputs => ({ ...inputs, [event.target.name]: Number(event.target.value) }))
   }
 
   const deleteProd = (params) => {
@@ -76,59 +70,65 @@ const elTotal = (carrito)=>{
 
 
   return (
-    <div className='contenedorCarrito'>
-      <h1 style={{textAlign: 'center'}}>Carrito de compras:</h1>
+    <div className="contenedorCarrito">
+      <h1 style={{ textAlign: 'center' }}>Carrito de compras:</h1>
       <hr />
-      {props.carrito.length ? <form className='inputCarrito' onSubmit={handleSubmit}>
-      <div>
-      <table className='tabla'>
-        <tbody>
-          <tr className='tr'>
-            <th>Imagenes</th>
-            <th>Productos</th>
-            <th>Cantidad</th>
-            <th>Precio unitario</th>
-            <th>Subtotal</th>
-          </tr>
-          {props.carrito.map( producto =>{
-            return(
-          <tr key={producto.id}>
-            <td><img style={{width: '5rem', height: '5rem', objectFit:'contain'}} src={producto.imagenes[0]}/></td>
-            <td><Link className='LinkCheckout' to={`/productos/${producto.id}`}>{producto.marca + ' '+ producto.modelo}</Link></td>
-            <td><input type="number" name={producto.id} id='cantidadProd' min='1' max={producto.stock} defaultValue={producto.carrito.cantidad ? producto.carrito.cantidad : 1} onChange={handleChange} />
-            <img onClick={() => deleteProd(producto.id)} value={producto.id} id='trashCarrito' title='Eliminar' src="/utils/cartdelete.svg"></img>
+      {props.carrito.length ? (
+        <form className="inputCarrito" onSubmit={handleSubmit}>
+          <div>
+            <table className="tabla">
+              <tbody>
+                <tr className="tr">
+                  <th>Imagenes</th>
+                  <th>Productos</th>
+                  <th>Cantidad</th>
+                  <th>Precio unitario</th>
+                  <th>Subtotal</th>
+                </tr>
+                {props.carrito.map(producto => (
+                  <tr key={producto.id}>
+                    <td><img style={{ width: '5rem', height: '5rem', objectFit: 'contain' }} src={producto.imagenes[0]} /></td>
+                    <td><Link className="LinkCheckout" to={`/productos/${producto.id}`}>{`${producto.marca} ${producto.modelo}`}</Link></td>
+                    <td>
+                      <input type="number" name={producto.id} id="cantidadProd" min="1" max={producto.stock} defaultValue={producto.carrito.cantidad ? producto.carrito.cantidad : 1} onChange={handleChange} />
+                      <img onClick={() => deleteProd(producto.id)} value={producto.id} id="trashCarrito" title="Eliminar" src="/utils/cartdelete.svg" />
 
-            </td>
-            <td>${producto.precio}</td>
-            <td>{inputs[producto.id] ? '$'+numberToString(stringToNumber(producto.precio) * inputs[producto.id]) : '$'+producto.precio}</td>
-          </tr>
-          
-          )
-          })}
-          <tr style={{ borderTop: 'solid thin darkslategrey'}}><td/><td/><td></td><td/><td  style={{fontWeight : '600'}}>{'$'+numberToString(Object.keys(inputs).length ? Total : elTotal(props.carrito))}</td></tr>
-        </tbody>
-      </table>
+                    </td>
+                    <td>
+                      $
+{producto.precio}
+                    </td>
+                    <td>{inputs[producto.id] ? `$${numberToString(stringToNumber(producto.precio) * inputs[producto.id])}` : `$${producto.precio}`}</td>
+                  </tr>
+
+                ))}
+                <tr style={{ borderTop: 'solid thin darkslategrey' }}>
+                  <td />
+                  <td />
+                  <td />
+                  <td />
+                  <td style={{ fontWeight: '600' }}>{`$${numberToString(Object.keys(inputs).length ? Total : elTotal(props.carrito))}`}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <button className="example_b botonCarrito general" type="submit" onSubmit={handleSubmit}>Comprar</button>
+        </form>
+      ) : ''}
     </div>
-        <button className='example_b botonCarrito general' type='submit' onSubmit={handleSubmit}>Comprar</button>
-      </form> : ''}
-    </div>
-  );
+  )
 }
 
-const mapStateToProps = state => {
-  return {
-    carrito: state.carrito,
-    productos: state.products,
-    usuario: state.usuario
-  };
-};
+const mapStateToProps = state => ({
+  carrito: state.carrito,
+  productos: state.products,
+  usuario: state.usuario,
+})
 
-const mapDispatchToProps = dispatch => {
-  return {
-    getCarrito: (id) => dispatch(fetchCarrito(id)),
-    updateCarrito: (id, cantidad, productos) => dispatch(updateCarrito(id, cantidad, productos)),
-  };
-}
+const mapDispatchToProps = dispatch => ({
+  getCarrito: (id) => dispatch(fetchCarrito(id)),
+  updateCarrito: (id, cantidad, productos) => dispatch(updateCarrito(id, cantidad, productos)),
+})
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(CarritoContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(CarritoContainer)
